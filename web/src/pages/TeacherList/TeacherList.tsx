fd8e19f1a9357e9a03614b2ba93839952b2520a4
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-06 17:36:36 -0300

    feat(web): Adding the server connection, and finished all the components and pages.

--- web/src/pages/TeacherList/TeacherList.tsx
+++ web/src/pages/TeacherList/TeacherList.tsx
@@ -1,37 +1,87 @@
-import React from 'react';
-import {PageHeader, TeacherItem} from "../../components";
+import React, {FormEvent, useState} from 'react';
+
+import {Input, PageHeader, Select, TeacherItem} from "../../components";
+import {Teacher} from "../../components/TeacherItem/TeacherItem";
+import {API} from "../../services";
 
 import './styles.css';
 
 const TeacherList = () => {
 
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    const [teachers, setTeachers] = useState([]);
+
+    async function handleSearchTeaches (e: FormEvent) {
+        e.preventDefault();
+
+        const response = await API.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data.classes)
+
+        console.log(teachers, response.data.classes)
+
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available profs.">
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Subject</label>
-                        <input type="text" id="subject" name="subject"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Day od the week</label>
-                        <input type="text" id="week_day" name="week_day"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Time</label>
-                        <input type="text" id="time" name="time"/>
-                    </div>
+                <form id="search-teachers" onSubmit={handleSearchTeaches}>
+                    <Select
+                        name="subject"
+                        label="Subject"
+                        value={subject}
+                        onChange={(e) => {setSubject(e.target.value)}}
+                        options={[
+                            { value: 'Arts', label: 'Arts' },
+                            { value: 'Science', label: 'Science' },
+                            { value: 'History', label: 'History' },
+                            { value: 'Physics', label: 'Physics' },
+                            { value: 'Education physics', label: 'Education' },
+                            { value: 'Geography', label: 'Geography' },
+                            { value: 'French', label: 'French' },
+                            { value: 'Spanish', label: 'Spanish' }
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Week day"
+                        value={week_day}
+                        onChange={(e) => {setWeekDay(e.target.value)}}
+                        options={[
+                            { value: '0', label: 'Sunday' },
+                            { value: '1', label: 'Monday' },
+                            { value: '2', label: 'Tuesday' },
+                            { value: '3', label: 'Wednesday' },
+                            { value: '4', label: 'Thursday' },
+                            { value: '5', label: 'Friday' },
+                            { value: '6', label: 'Saturday' }
+                        ]}
+                    />
+                    <Input
+                        type="time"
+                        name="time"
+                        label="Time"
+                        value={time}
+                        onChange={(e) => {setTime(e.target.value)}}
+                    />
+                    <button type="submit">Search</button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher}/>
+                })}
+
             </main>
         </div>
     );
