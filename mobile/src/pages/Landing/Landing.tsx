68d5f366fba33b2cf50b01aba063844cf72a81da
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-07 20:48:22 -0300

    feat(mobile): Creation of the Landing page.

---
+++ mobile/src/pages/Landing/Landing.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import landingImage from "../../assets/images/landing.png";
+import studyIcon from "../../assets/images/icons/study.png";
+import giveClassesIcon from "../../assets/images/icons/give-classes.png";
+import heartIcon from "../../assets/images/icons/heart.png";
+
+interface LandingProps {}
+
+const Landing = ({}: LandingProps) => {
+
+    return (
+        <View style={styles.container}>
+            <Image source={landingImage} style={styles.banner} />
+
+            <Text style={styles.title}>
+                Welcome, {"\n"}
+                <Text style={styles.titleBold}>What do you want to do?</Text>
+            </Text>
+            <View style={styles.buttonsContainer}>
+                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Study</Text>
+                </RectButton>
+
+                <RectButton style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Teach</Text>
+                </RectButton>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                More than 300 connections done {" "}
+                <Image source={heartIcon} />
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#8257E5",
+        justifyContent: "center",
+        padding: 40
+    },
+
+    banner : {
+        width: "100%",
+        resizeMode: "contain"
+    },
+
+    title: {
+        fontFamily: "Poppins_400Regular",
+        color: "#FFF",
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80
+    },
+
+    titleBold: {
+        fontFamily: "Poppins_600SemiBold"
+    },
+
+    buttonsContainer: {
+        flexDirection: "row",
+        marginTop: 40,
+        justifyContent: "space-between"
+    },
+
+    button: {
+        height: 120,
+        width: "48%",
+        backgroundColor: "#333",
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: "space-between"
+    },
+
+    buttonPrimary: {
+        backgroundColor: "#9871F5"
+    },
+
+    buttonSecondary: {
+        backgroundColor: "#04D361"
+    },
+
+    buttonText: {
+        fontFamily: "Archivo_700Bold",
+        color: "#FFF",
+        fontSize: 20
+    },
+
+    totalConnections: {
+        fontFamily: "Poppins_400Regular",
+        color: "#D4CDFF",
+        fontSize: 14,
+        lineHeight: 20,
+        // maxWidth: 140,
+        marginTop: 40
+    }
+});
+
+export default Landing;
